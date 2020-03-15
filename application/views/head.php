8d01d803b71d2ec5708891bb7c0bec715aeaea05
smith <andras@sexybuddha.hu>
2020-03-15 11:51:53 +0100

    link back to repo

--- application/views/head.php
+++ application/views/head.php
@@ -6,21 +6,21 @@ $_title = 'm3 kereshető archívum';
 ?><!DOCTYPE html>
 <html>
 <head>
-	<title><?php echo $_title; ?></title>
+    <title><?php echo $_title; ?></title>
 
-	<meta charset="utf-8">
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  	<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
-	<link href="https://unpkg.com/@mdi/font@latest/css/materialdesignicons.min.css" rel="stylesheet">
-	<link href="public/mwc.min.css" rel="stylesheet">
-	<link href="public/app.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" rel="stylesheet">
+    <link href="https://unpkg.com/@mdi/font@latest/css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="public/mwc.min.css" rel="stylesheet">
+    <link href="public/app.css" rel="stylesheet">
 
 </head>
 <body class="mdc-typography">
 
-	<header>
-		<h1 class="mdc-typography--headline5"><?php echo $_title; ?></h1>
-	</header>
+    <header>
+        <h1 class="mdc-typography--headline5"><?php echo $_title; ?></h1>
+    </header>
 
-	<main class="main-content mdc-typography--body2">
+    <main class="main-content mdc-typography--body2">
